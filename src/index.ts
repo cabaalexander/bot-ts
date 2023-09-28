ef687d15b5d40cb39417f8a388debd2917160f7a
cabaalexander <cabaalexander321@gmail.com>
2023-09-28 15:59:02 -0400

    set github actions for deploying to CF
    * change hello message to reflect deploy

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import {Hono} from 'hono';
 
 const app = new Hono();
 
-app.get('/', c => c.text('Hello world Hono! (lint)'));
+app.get('/', c => c.text('Hello world Hono! (lint) [action]'));
 
 export default app;

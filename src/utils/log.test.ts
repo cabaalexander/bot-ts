316807a9149a671899146d3c57dddaeb175fe16b
cabaalexander <cabaalexander321@gmail.com>
2023-10-04 18:50:22 -0400

    remove feature to not print from logger

--- src/utils/log.test.ts
+++ src/utils/log.test.ts
@@ -1,7 +1,5 @@
 import {logError, logInfo} from './log';
 
-const env = getMiniflareBindings();
-
 describe('log', () => {
   let log: jest.SpyInstance;
   let error: jest.SpyInstance;
@@ -17,22 +15,10 @@ describe('log', () => {
   });
 
   it('should log info provided', () => {
-    env.NO_LOG = undefined;
-
     logInfo('hello info');
     expect(log).toBeCalledWith('--[info]', 'hello info');
 
     logError('hello error');
     expect(error).toBeCalledWith('hello error');
-  });
-
-  it('should not log', () => {
-    env.NO_LOG = '1';
-
-    logInfo('zup');
-    expect(log).not.toBeCalled();
-
-    logError('m8');
-    expect(error).not.toBeCalled();
   });
 });
--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,23 +1,13 @@
 import {z} from 'zod';
 
-const env = getMiniflareBindings();
-
 const logSchema = z.array(z.unknown());
 
 type Log = z.infer<typeof logSchema>;
 
 export function logInfo(...args: Log): void {
-  if (env.NO_LOG) {
-    return;
-  }
-
   console.log('--[info]', ...args);
 }
 
 export function logError(...args: Log) {
-  if (env.NO_LOG) {
-    return;
-  }
-
   console.error(...args);
 }

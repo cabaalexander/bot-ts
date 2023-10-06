1934cedae7905dbffcfa30fff5da46d78b323adb
cabaalexander <cabaalexander321@gmail.com>
2023-10-06 00:43:48 -0400

    keep encapsulation simple (for now) move 'api' code to 'index'

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-import app from './api';
+import {Hono} from 'hono';
+import {type Bindings} from './config/types';
+import verifyDiscordRequest from './middlewares/verify-discord-request';
+import discordPing from './middlewares/discord-ping';
+
+const app = new Hono<{Bindings: Bindings}>();
+
+app.use(verifyDiscordRequest(), discordPing());
 
 app.get('/', c => c.text('👋 Hello there!'));
 
